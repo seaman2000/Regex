9a11ca9d6aa1be05b31ae0424ca81264d6271596
Georgi Angelov <gtangelov76@gmail.com>
2026-03-11 21:52:32 +0200

    Update match_phone_number.py

--- Regex_Lab/match_phone_number.py
+++ Regex_Lab/match_phone_number.py
@@ -2,7 +2,7 @@ import re
 
 phone_numbers = input()
 
-pattern = r'\+359 \d{1} \d{3} \d{4}|\+359-\d{1}-\d{3}-\d{4}'
+pattern = r'\+359 \d{1} \d{3} \d{4}|\+359-\d{1}-\d{3}-\d{4}\b'
 
 result = re.findall(pattern, phone_numbers)
 
