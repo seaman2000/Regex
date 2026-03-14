e89c251eaab68108c08b593f0a2fc673c11b3ecf
Georgi Angelov <gtangelov76@gmail.com>
2026-03-14 19:56:43 +0200

    Update extract_the_links.py

--- Regex_Exercise/extract_the_links.py
+++ Regex_Exercise/extract_the_links.py
@@ -1,14 +1,14 @@
 import re
 
 
-pattern = r'(www.[a-z][a-z\d\-]+(\.[a-z]{1,}+){1,})'
+pattern = r"(w{3}\.[a-z\d\-]+(\.[a-z]+)+)"
 text_with_links = input()
 
 while text_with_links:
-
     found_links = re.search(pattern, text_with_links, re.IGNORECASE)
 
     if found_links:
         link = found_links.group(1)
         print(link)
+
     text_with_links = input()
