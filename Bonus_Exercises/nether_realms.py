d7a2b4ae7b8988f65d1e6de956ab2b8bffab5d27
Georgi Angelov <gtangelov76@gmail.com>
2026-03-15 20:05:58 +0200

    Update nether_realms.py

--- Bonus_Exercises/nether_realms.py
+++ Bonus_Exercises/nether_realms.py
@@ -1,6 +1,6 @@
 import re
 
-demon_names = input().split(', ')
+demon_names = re.split(r'\s*,\s*', input())
 
 pattern_for_name = r'[^0-9\+\-\*\/.]'
 pattern_for_damage = r'[+\-]*\d[\.\d]*'
@@ -15,7 +15,7 @@ for name in demon_names:
         demon_health += ord(char)
 
     extracted_damage = re.findall(pattern_for_damage, name)
-    extracted_damage = [int(damage) for damage in extracted_damage]
+    extracted_damage = [float(damage) for damage in extracted_damage]
     dividers_or_multipliers = re.findall(pattern_for_divide_or_multiply, name)
     damage_sum = sum(extracted_damage)
 
@@ -26,8 +26,8 @@ for name in demon_names:
             damage_sum /= 2
     demon_dict[name] = demon_health, damage_sum
 
-for demon in sorted(demon_dict.keys()):
-    print(f"{demon} - {demon[0]} health, {demon[1]} damage")
+for demon, (health, dmg) in sorted(demon_dict.items()):
+    print(f"{demon} - {health} health, {dmg:.2f} damage")
 
 
 
