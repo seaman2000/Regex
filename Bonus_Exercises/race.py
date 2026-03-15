b3826af812659427f23b3901c518d2fe31622845
Georgi Angelov <gtangelov76@gmail.com>
2026-03-15 13:53:39 +0200

    Update race.py

--- Bonus_Exercises/race.py
+++ Bonus_Exercises/race.py
@@ -1,11 +1,12 @@
 import re
 
-pattern = r''
+pattern_for_name = r''
+pattern_for_numbers = r''
 
 participants = input().split(', ')
 line = input()
 
 while line != "end of race":
-    
+
 
     line = input()
