55185e7cc8f58e693a5ad6a5f09b69718ec367e1
Georgi Angelov <gtangelov76@gmail.com>
2026-03-20 13:50:09 +0200

    Update html_parser.py

--- Bonus_Exercises/html_parser.py
+++ Bonus_Exercises/html_parser.py
@@ -1,7 +1,14 @@
 import re
 html_input = input()
 
-title_pattern = r"<title>([^|]+)</title>"
-content_pattern = r"<body>([^|]+)</body>"
-text_pattern = r""
+title_pattern = r"<title>(.*?)</title>"
+content_pattern = r"<body>(.*?)</body>"
+text_pattern = r"<[^>]+>|\\n"
+
+title = ''.join(re.findall(title_pattern, html_input))
+content = re.search(content_pattern, html_input).group(1)
+text = re.sub(text_pattern, '', content)
+
+print(f"Title: {title}\n"
+      f"Content: {text}")
 
